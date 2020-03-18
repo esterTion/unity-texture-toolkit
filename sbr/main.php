dd3d9403c51c77e37c0343b8e5130543664f0de6
esterTion <lcz970@126.com>
2020-03-18 14:06:24 +0800

    SBR-FAL tools indent to space

--- sbr/main.php
+++ sbr/main.php
@@ -87,7 +87,7 @@ function prettifyJSON($in, Stream $out = NULL, $returnData = true) {
 
 
 function main () {
-	global $last_version;
+    global $last_version;
 
     /*
     $appver = file_exists('appver') ? file_get_contents('appver') : '2.1.0';
@@ -173,13 +173,13 @@ function main () {
         CURLOPT_POSTFIELDS=>msgpack_pack($request_body)
     ]);
 
-	$response = msgpack_unpack(curl_exec($curl));
-	//$response = msgpack_unpack(file_get_contents("title-game_start.dat"));
+    $response = msgpack_unpack(curl_exec($curl));
+    //$response = msgpack_unpack(file_get_contents("title-game_start.dat"));
     $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
     if (empty($response['body']['session_key'])) {
-		_log("login failed $code");
-		return false;
+        _log("login failed $code");
+        return false;
     }
     $userid = $response['body']['user']['id'];
     $session = $response['body']['session_key'];
@@ -201,130 +201,130 @@ function main () {
     ]);
 
     $response = msgpack_unpack(curl_exec($curl));
-	//$response = msgpack_unpack(file_get_contents("masterdata-tables.dat"));
+    //$response = msgpack_unpack(file_get_contents("masterdata-tables.dat"));
     $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-	print_r($response);
-	
-	if (empty($response['body']['table_versions'])) {
-		_log("get masterdata failed $code");
-		return false;
-	}
-	$updated_tables = [];
-	foreach ($response['body']['table_versions'] as $table) {
-		$name = $table['table_name'];
-		$version = $table['version'];
-		if (!empty($last_version[$name]) && $last_version[$name] == $version) {
-			continue;
-		}
-		$updated_tables[$name] = $version;
-	}
-	print_r($updated_tables);
+    print_r($response);
+    
+    if (empty($response['body']['table_versions'])) {
+        _log("get masterdata failed $code");
+        return false;
+    }
+    $updated_tables = [];
+    foreach ($response['body']['table_versions'] as $table) {
+        $name = $table['table_name'];
+        $version = $table['version'];
+        if (!empty($last_version[$name]) && $last_version[$name] == $version) {
+            continue;
+        }
+        $updated_tables[$name] = $version;
+    }
+    print_r($updated_tables);
 
-	if (empty($updated_tables)) {
-		_log('no update found');
-		return false;
-	}
+    if (empty($updated_tables)) {
+        _log('no update found');
+        return false;
+    }
 
-	$url = "masterdata/records";
-	foreach ($updated_tables as $name=>$version) {
-		$request_body = [
-			'url' => $url,
-			"user_id" => $userid,
-			'table_name' => $name,
-			"session_key" => $session,
-			"platform" => 1,
-			"app_version" => 10000000,
-			"masterdata_version" => $version,
-			"retry_count" => 0,
-			"request_hash" => floor(microtime(true) * 1000)
-		];
-		curl_setopt_array($curl, [
-			CURLOPT_URL=>"https://prod1-api.showbyrock-fes.com/$url",
-			CURLOPT_POSTFIELDS=>msgpack_pack($request_body)
-		]);
-	
-		$response = msgpack_unpack(curl_exec($curl));
-		$code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		if (empty($response['body']['records'])) {
-			_log("download $name failed $code");
-			continue;
-		}
-		fclose(fopen("data/$name.json", 'w'));
-		$out = new FileStream("data/$name.json");
-		prettifyJSON(json_encode($response['body']['records'], JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES), $out, false);
-		$last_version[$name] = $version;
-	}
-	file_put_contents('last_version', json_encode($last_version));
-	file_put_contents('data/!version.json', json_encode($last_version, JSON_PRETTY_PRINT));
-	return true;
+    $url = "masterdata/records";
+    foreach ($updated_tables as $name=>$version) {
+        $request_body = [
+            'url' => $url,
+            "user_id" => $userid,
+            'table_name' => $name,
+            "session_key" => $session,
+            "platform" => 1,
+            "app_version" => 10000000,
+            "masterdata_version" => $version,
+            "retry_count" => 0,
+            "request_hash" => floor(microtime(true) * 1000)
+        ];
+        curl_setopt_array($curl, [
+            CURLOPT_URL=>"https://prod1-api.showbyrock-fes.com/$url",
+            CURLOPT_POSTFIELDS=>msgpack_pack($request_body)
+        ]);
+    
+        $response = msgpack_unpack(curl_exec($curl));
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if (empty($response['body']['records'])) {
+            _log("download $name failed $code");
+            continue;
+        }
+        fclose(fopen("data/$name.json", 'w'));
+        $out = new FileStream("data/$name.json");
+        prettifyJSON(json_encode($response['body']['records'], JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES), $out, false);
+        $last_version[$name] = $version;
+    }
+    file_put_contents('last_version', json_encode($last_version));
+    file_put_contents('data/!version.json', json_encode($last_version, JSON_PRETTY_PRINT));
+    return true;
 }
 
 function GeneratePBKDF2Key($password, $salt) {
     return hash_pbkdf2('SHA1', $password, $salt, 1024, 32+16, true);
 }
 function getAssetUrl(string $path) {
-	$path = explode('/', trim($path, '/'));
-	$path = array_map(function ($i) {
-		return xxhash64($i, 0x33DC7CB65408BDF, true);
-	}, $path);
-	return implode('/', $path);
+    $path = explode('/', trim($path, '/'));
+    $path = array_map(function ($i) {
+        return xxhash64($i, 0x33DC7CB65408BDF, true);
+    }, $path);
+    return implode('/', $path);
 }
 function asset() {
-	global $last_version;
+    global $last_version;
     $curl = curl_init();
     curl_setopt_array($curl, [
         //CURLOPT_PROXY=>'127.0.0.1:50000',
         CURLOPT_RETURNTRANSFER=>true,
         CURLOPT_HEADER=>false,
         CURLOPT_SSL_VERIFYPEER=>false
-	]);
-	
-	curl_setopt($curl, CURLOPT_URL, 'https://prod-dlc-cache.showbyrock-fes.com/asset/'.getAssetUrl('ios/filelist'));
-	$assets_infos_enc = curl_exec($curl);
-	$hash = hash('sha1', $assets_infos_enc);
-	if (!empty($last_version['asset_infos']) && $last_version['asset_infos'] == $hash) {
-		return false;
-	}
-	_log('new asset versions');
-	$key = base64_decode('KAjyg6DTjC5ScfH9');
-	$salt = base64_decode('2Ae2C7jQEJuU6h2j');
-	$aesData = GeneratePBKDF2Key($key, $salt);
-	$assets_infos_dec = openssl_decrypt($assets_infos_enc, 'AES-256-CBC', substr($aesData, 0, 32), OPENSSL_RAW_DATA, substr($aesData, 32, 16));
-	$s = new MemoryStream($assets_infos_dec);
-	$s->littleEndian = true;
-	if ($s->ulong != 0xBFC2A1C2) {
-		_log('invalid asset info format');
-		return false;
-	}
-	$s->ulong;
-	$assets = [];
-	while ($s->position < $s->size) {
-		$name = $s->readData($s->ulong);
-		$assets[$name] = [
-			'hash' => $s->ulonglong,
-			'size' => $s->long,
-			'category' => $s->long,
-			'is_individual_download' => ord($s->byte)
-		];
-	}
-	ksort($assets);
-	fclose(fopen("data/asset_infos.json", 'w'));
-	$out = new FileStream("data/asset_infos.json");
-	prettifyJSON(json_encode($assets, JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES), $out, false);
-	$last_version['asset_infos'] = $hash;
-	file_put_contents('last_version', json_encode($last_version));
-	file_put_contents('data/!version.json', json_encode($last_version, JSON_PRETTY_PRINT));
+    ]);
+    
+    curl_setopt($curl, CURLOPT_URL, 'https://prod-dlc-cache.showbyrock-fes.com/asset/'.getAssetUrl('ios/filelist'));
+    $assets_infos_enc = curl_exec($curl);
+    $hash = hash('sha1', $assets_infos_enc);
+    if (!empty($last_version['asset_infos']) && $last_version['asset_infos'] == $hash) {
+        return false;
+    }
+    _log('new asset versions');
+    $key = base64_decode('KAjyg6DTjC5ScfH9');
+    $salt = base64_decode('2Ae2C7jQEJuU6h2j');
+    $aesData = GeneratePBKDF2Key($key, $salt);
+    $assets_infos_dec = openssl_decrypt($assets_infos_enc, 'AES-256-CBC', substr($aesData, 0, 32), OPENSSL_RAW_DATA, substr($aesData, 32, 16));
+    $s = new MemoryStream($assets_infos_dec);
+    $s->littleEndian = true;
+    if ($s->ulong != 0xBFC2A1C2) {
+        _log('invalid asset info format');
+        return false;
+    }
+    $s->ulong;
+    $assets = [];
+    while ($s->position < $s->size) {
+        $name = $s->readData($s->ulong);
+        $assets[$name] = [
+            'hash' => $s->ulonglong,
+            'size' => $s->long,
+            'category' => $s->long,
+            'is_individual_download' => ord($s->byte)
+        ];
+    }
+    ksort($assets);
+    fclose(fopen("data/asset_infos.json", 'w'));
+    $out = new FileStream("data/asset_infos.json");
+    prettifyJSON(json_encode($assets, JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES), $out, false);
+    $last_version['asset_infos'] = $hash;
+    file_put_contents('last_version', json_encode($last_version));
+    file_put_contents('data/!version.json', json_encode($last_version, JSON_PRETTY_PRINT));
 }
 
 $dbUpdate = main();
 $assetUpdate = asset();
 if ($dbUpdate || $assetUpdate) {
-	chdir('data');
-	exec('git add .');
-	exec('git commit -m update');
-	exec('git push origin master');
-	chdir(__DIR__);
+    chdir('data');
+    exec('git add .');
+    exec('git commit -m update');
+    exec('git push origin master');
+    chdir(__DIR__);
 }
 if ($assetUpdate) {
-	// checkAndUpdateResource();
+    // checkAndUpdateResource();
 }
